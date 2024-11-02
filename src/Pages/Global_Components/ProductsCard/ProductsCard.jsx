abb0fd0adc309d0ef93d497e0f45557c50ad4758
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-11-02 20:25:28 +0600

    store cards end and card updated

--- src/Pages/Global_Components/ProductsCard/ProductsCard.jsx
+++ src/Pages/Global_Components/ProductsCard/ProductsCard.jsx
@@ -1,29 +1,41 @@
 import React from 'react'
 import styles from "./ProductsCard.module.css"
-import img from "../../../assets/art_img.jpg"
 import { MdFavoriteBorder } from "react-icons/md";
-export default function ProductsCard() {
-    const price=100
-    const like="10k"
+export default function ProductsCard({card_data}) {
+     let love_react=0
+     if(card_data.like<1000){
+      love_react=card_data.like
+     }else if(card_data.like<1000000){
+      love_react=card_data.like/1000 + "K"
+     }
+     else{
+      love_react=card_data.like/1000000 +"M"
+     }
+     const Buynow=()=>{
+       console.log(card_data)
+     }
+     const AddToCart=()=>{
+      console.log(card_data)
+     }
   return (
     <div className={styles.card}>
        <div className={styles.img_div}>
-        <img src={img} className={styles.img}/>
+        <img src={card_data.img} className={styles.img}/>
        </div>
        <div className={styles.card_bio}>
           <div className={styles.card_pricing_buying}>
             <div className={styles.card_pricing}>
-               <div className={styles.love_status}> <p className={styles.love_icon}>< MdFavoriteBorder  /> </p>{like}</div>
-               <div className={styles.pricing}>Price: <p className={styles.pricing_amount}> {price}৳</p></div>
-               <p className={styles.artavailable}><b>Available</b></p>
+               <div className={styles.love_status}> <p className={styles.love_icon}>< MdFavoriteBorder  /> </p>{love_react}</div>
+               <div className={styles.pricing}>Price: <p className={styles.pricing_amount}> {card_data.price}৳</p></div>
+               <p className={styles.artavailable}><b>{card_data.available}</b></p>
             </div>
             <div className={styles.buy_add_cart}>
-              <button className={styles.buy_btn}>Buy Now</button>
-              <button className={styles.cart_btn}>Add to Cart</button>
+              <button className={styles.buy_btn} onClick={Buynow}>Buy Now</button>
+              <button className={styles.cart_btn} onClick={AddToCart}>Add to Cart</button>
             </div>
           </div>
           <div className={styles.cart_detiels}>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam libero autem eius.</p>
+              <p>{card_data.bio}</p>
           </div>
        </div>
     </div>
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -2,11 +2,42 @@ import Nav from "../Global_Components/Nav/Nav"
 import ProductsCard from "../Global_Components/ProductsCard/ProductsCard"
 import styles from "./Home.module.css"
 import React from 'react'
+import img from "../../assets/art_img.jpg"
+const card_datas=[{
+  "img":img,
+  "like":999,
+  "price":250,
+  "available":"Available",
+  "bio":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam libero autem eius."
+},{
+  "img":img,
+  "like":20000,
+  "price":290,
+  "available":"Not Available",
+  "bio":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam libero autem eius."
+},{
+  "img":img,
+  "like":1000,
+  "price":2500,
+  "available":"Available",
+  "bio":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam libero autem eius."
+},
+{ "img":img,
+  "like":2000000,
+  "price":3680,
+  "available":"Available",
+  "bio":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam libero autem eius."
+}]
 export default function Home() {
   return (
     <div className={styles.hello}>
       <Nav/>
-      <ProductsCard />
+      <div className={styles.store}>
+        {
+          card_datas.map((item,i)=><ProductsCard card_data={item}/>
+          )
+        }
+      </div>
     </div>
   )
 }
